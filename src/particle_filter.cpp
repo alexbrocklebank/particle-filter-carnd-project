be42e1454abeb6a72146c751fb55d6ef5e4a6e51
alexbrocklebank <alexbrocklebank@gmail.com>
2017-12-12 20:54:27 -0500

    Weights vector initialization change, increment selector changed to include particles for weights update

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -51,7 +51,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	}
 
 	// Vector of weights of all particles
-	std::vector<double> weights(num_particles, 1.0);
+	weights(num_particles, 1.0);
 
 	// Flag, if filter is initialized
 	is_initialized = true;
@@ -198,7 +198,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 			// Update particle weights and weights vector
 			particles[p].weight = weight;
 			std::cout << "Particle " << p << " Weights Updated.\n";
-			weights[p] = weight;
+			weights[particles[p].id] = weight;
 			std::cout << "Weights Updated.\n";
 		}
 	}
